8e1413b70006a967b50157b3f5f3f8985874d85e
Ravneet Singh <44868830+ravneet98@users.noreply.github.com>
2024-05-04 12:07:52 -0400

    first Commit

--- main.tf
+++ main.tf
@@ -55,7 +55,7 @@ resource "azurerm_network_security_rule" "mtc-dev-rule" {
   protocol                    = "*"
   source_port_range           = "*"
   destination_port_range      = "*"
-  source_address_prefix       = "99.234.108.61/32"
+  source_address_prefix       = "my ip"
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.mtc-rg.name
   network_security_group_name = azurerm_network_security_group.mtc-sg.name
